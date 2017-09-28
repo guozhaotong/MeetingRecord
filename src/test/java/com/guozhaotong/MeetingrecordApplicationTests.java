7c18e858ade25c9e7857876982e7fdb4b325feae
guozhaotong <xjtuguozhaotong@qq.com>
2017-09-28 21:17:15 +0800

    创建了工程。

---
+++ src/test/java/com/guozhaotong/MeetingrecordApplicationTests.java
@@ -0,0 +1,16 @@
+package com.guozhaotong;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeetingrecordApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
